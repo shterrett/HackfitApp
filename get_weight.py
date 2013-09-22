acbb95866c5785f90168a9d0bcf41afc790b4537
Stuart Terrett <shterrett@gmail.com>
2013-09-21 22:16:47 -0400

    refactors input; handles weight naively

---
+++ get_weight.py
@@ -0,0 +1,2 @@
+def get_weight(data):
+  return sum(data) / length(data)
--- main.py
+++ main.py
@@ -1,33 +1,22 @@
-import serial
-import data_in
+import data_in_direct
 import smooth_curve
 import find_peaks
+import get_weight
 
 # data is expected in the form of [time, x-accel, y-accel, z-accel] into 
 # data-in. data_in returns a list of z-accel only.
 
-#data = data_in.start_server()
-addr  = '/dev/tty.usbmodemfd121'
-baud  = 9600
-fname = 'accel.csv'
-fmode = 'ab'
-reps  = 1000
-data = []
-
-with serial.Serial(addr,baud) as port:
-    print port.readline()
-    print port.readline()
-    print port.readline()
-    for i in range(reps):
-        x = port.readline();
-        vals = x.split(',');
-        print vals
-        data.append(vals[2]);
-        #print x
-        #outf.write(x)
-        #outf.flush()
-
-
-smooth_data = smooth_curve.smoothListGaussian(data)
+data = data_in_direct.start_collection()
+accel_data = get_accel_data(data)
+weight_data = get_weight_data(data)
+smooth_data = smooth_curve.smoothListGaussian(accel_data)
 peaks = find_peaks.find_peaks(smooth_data)
+weight = get_weight.get_weight(weight_dat)
 print peaks
+print weight
+
+def get_accel_data(data):
+  return [x[0] for x in data]
+
+def get_weight_data(data):
+  return [x[1] for x in data]
